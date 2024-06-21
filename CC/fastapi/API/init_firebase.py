a2abc4d8e0dd3d320cb18dbbebd42aa5a287fd36
dikafdp <ikawidyaningsih5@gmail.com>
2024-06-21 17:29:11 +0700

    CC comin

--- CC/fastapi/API/init_firebase.py
+++ CC/fastapi/API/init_firebase.py
@@ -1,6 +1,6 @@
 from firebase_admin import credentials, initialize_app, firestore, storage
 
-# Path to your Firebase service account key
+# Path Firebase service account key
 cred = credentials.Certificate("serviceaccountkey.json")
 app = initialize_app(cred, {'storageBucket': 'flash-parity-424914-r9.appspot.com'})
 
