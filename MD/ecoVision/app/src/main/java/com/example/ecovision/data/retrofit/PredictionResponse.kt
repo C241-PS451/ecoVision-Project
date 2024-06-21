598432450092b16b660b9ff87397d3bc00aef7ef
Ananda Nurrizki <108229646+Krokorilla@users.noreply.github.com>
2024-06-21 14:50:40 +0700

    Delete MD/ecoVision directory

--- MD/ecoVision/app/src/main/java/com/example/ecovision/data/retrofit/PredictionResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ecovision.data.retrofit
-
-import com.google.gson.annotations.SerializedName
-
-data class PredictionResponse(
-    @SerializedName("predicted_class") val predictedClass: String,
-    @SerializedName("image_url") val imageUrl: String,
-    @SerializedName("prediction_time") val predictionTime: String
-)
--- MD/ecoVision/app/src/main/java/com/example/ecovision/util/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ecovision.util
-
-object Constants {
-    const val MODEL_PATH = "ecovisionmodelv4.tflite"
-    const val LABELS_PATH = "labels.txt"
-}
